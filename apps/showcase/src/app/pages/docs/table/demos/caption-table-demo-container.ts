fa14123158e1378fa6bca7ebd90f8aa99545a090
kgridou <32600911+kgridou@users.noreply.github.com>
2026-01-22 20:37:37 +0100

    wip

--- apps/showcase/src/app/pages/docs/table/demos/caption-table-demo-container.ts
+++ apps/showcase/src/app/pages/docs/table/demos/caption-table-demo-container.ts
@@ -6,30 +6,67 @@ import { CaptionTableDemo } from './caption-table-demo';
   selector: 'app-caption-table-demo-container',
   imports: [DemoContainer, CaptionTableDemo],
   template: `
-    <app-demo-container title="With Caption" [code]="code">
+    <app-demo-container
+      title="With Caption"
+      [code]="code"
+      language="typescript"
+    >
       <app-caption-table-demo />
     </app-demo-container>
   `,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export default class CaptionTableDemoContainer {
-  readonly code = `<table sc-table>
-  <caption sc-table-caption>A list of your recent invoices.</caption>
-  <thead sc-table-header>
-    <tr sc-table-row>
-      <th sc-table-head class="w-[100px]">Invoice</th>
-      <th sc-table-head>Status</th>
-      <th sc-table-head>Method</th>
-      <th sc-table-head class="text-right">Amount</th>
-    </tr>
-  </thead>
-  <tbody sc-table-body>
-    <tr sc-table-row>
-      <td sc-table-cell class="font-medium">INV001</td>
-      <td sc-table-cell>Paid</td>
-      <td sc-table-cell>Credit Card</td>
-      <td sc-table-cell class="text-right">$250.00</td>
-    </tr>
-  </tbody>
-</table>`;
+  readonly code = `import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {
+  ScTable,
+  ScTableBody,
+  ScTableCaption,
+  ScTableCell,
+  ScTableHead,
+  ScTableHeader,
+  ScTableRow,
+} from '@semantic-components/ui';
+
+@Component({
+  selector: 'app-caption-table-demo',
+  imports: [
+    ScTable,
+    ScTableBody,
+    ScTableCaption,
+    ScTableCell,
+    ScTableHead,
+    ScTableHeader,
+    ScTableRow,
+  ],
+  template: \`
+    <table sc-table>
+      <caption sc-table-caption>A list of your recent invoices.</caption>
+      <thead sc-table-header>
+        <tr sc-table-row>
+          <th sc-table-head class="w-[100px]">Invoice</th>
+          <th sc-table-head>Status</th>
+          <th sc-table-head>Method</th>
+          <th sc-table-head class="text-right">Amount</th>
+        </tr>
+      </thead>
+      <tbody sc-table-body>
+        <tr sc-table-row>
+          <td sc-table-cell class="font-medium">INV001</td>
+          <td sc-table-cell>Paid</td>
+          <td sc-table-cell>Credit Card</td>
+          <td sc-table-cell class="text-right">$250.00</td>
+        </tr>
+        <tr sc-table-row>
+          <td sc-table-cell class="font-medium">INV002</td>
+          <td sc-table-cell>Pending</td>
+          <td sc-table-cell>PayPal</td>
+          <td sc-table-cell class="text-right">$150.00</td>
+        </tr>
+      </tbody>
+    </table>
+  \`,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class CaptionTableDemo {}`;
 }
--- apps/showcase/src/app/pages/docs/table/demos/footer-table-demo-container.ts
+++ apps/showcase/src/app/pages/docs/table/demos/footer-table-demo-container.ts
@@ -6,35 +6,74 @@ import { FooterTableDemo } from './footer-table-demo';
   selector: 'app-footer-table-demo-container',
   imports: [DemoContainer, FooterTableDemo],
   template: `
-    <app-demo-container title="With Footer" [code]="code">
+    <app-demo-container title="With Footer" [code]="code" language="typescript">
       <app-footer-table-demo />
     </app-demo-container>
   `,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export default class FooterTableDemoContainer {
-  readonly code = `<table sc-table>
-  <thead sc-table-header>
-    <tr sc-table-row>
-      <th sc-table-head class="w-[100px]">Invoice</th>
-      <th sc-table-head>Status</th>
-      <th sc-table-head>Method</th>
-      <th sc-table-head class="text-right">Amount</th>
-    </tr>
-  </thead>
-  <tbody sc-table-body>
-    <tr sc-table-row>
-      <td sc-table-cell class="font-medium">INV001</td>
-      <td sc-table-cell>Paid</td>
-      <td sc-table-cell>Credit Card</td>
-      <td sc-table-cell class="text-right">$250.00</td>
-    </tr>
-  </tbody>
-  <tfoot sc-table-footer>
-    <tr sc-table-row>
-      <td sc-table-cell colspan="3">Total</td>
-      <td sc-table-cell class="text-right">$250.00</td>
-    </tr>
-  </tfoot>
-</table>`;
+  readonly code = `import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {
+  ScTable,
+  ScTableBody,
+  ScTableCell,
+  ScTableFooter,
+  ScTableHead,
+  ScTableHeader,
+  ScTableRow,
+} from '@semantic-components/ui';
+
+@Component({
+  selector: 'app-footer-table-demo',
+  imports: [
+    ScTable,
+    ScTableBody,
+    ScTableCell,
+    ScTableFooter,
+    ScTableHead,
+    ScTableHeader,
+    ScTableRow,
+  ],
+  template: \`
+    <table sc-table>
+      <thead sc-table-header>
+        <tr sc-table-row>
+          <th sc-table-head class="w-[100px]">Invoice</th>
+          <th sc-table-head>Status</th>
+          <th sc-table-head>Method</th>
+          <th sc-table-head class="text-right">Amount</th>
+        </tr>
+      </thead>
+      <tbody sc-table-body>
+        <tr sc-table-row>
+          <td sc-table-cell class="font-medium">INV001</td>
+          <td sc-table-cell>Paid</td>
+          <td sc-table-cell>Credit Card</td>
+          <td sc-table-cell class="text-right">$250.00</td>
+        </tr>
+        <tr sc-table-row>
+          <td sc-table-cell class="font-medium">INV002</td>
+          <td sc-table-cell>Pending</td>
+          <td sc-table-cell>PayPal</td>
+          <td sc-table-cell class="text-right">$150.00</td>
+        </tr>
+        <tr sc-table-row>
+          <td sc-table-cell class="font-medium">INV003</td>
+          <td sc-table-cell>Paid</td>
+          <td sc-table-cell>Bank Transfer</td>
+          <td sc-table-cell class="text-right">$350.00</td>
+        </tr>
+      </tbody>
+      <tfoot sc-table-footer>
+        <tr sc-table-row>
+          <td sc-table-cell colspan="3">Total</td>
+          <td sc-table-cell class="text-right">$750.00</td>
+        </tr>
+      </tfoot>
+    </table>
+  \`,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class FooterTableDemo {}`;
 }
