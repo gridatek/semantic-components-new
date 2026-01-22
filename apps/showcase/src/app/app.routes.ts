d7a2a067726b9e7e0aff844310494588589ff0c6
kgridou <32600911+kgridou@users.noreply.github.com>
2026-01-22 19:32:21 +0100

    wip

--- apps/showcase/src/app/app.routes.ts
+++ apps/showcase/src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { componentsRoutes } from './routes/components.routes';
 import { demosRoutes } from './routes/demos.routes';
 
 export const appRoutes: Route[] = [
+  ...demosRoutes,
   {
     path: '',
     component: StackedLayout,
@@ -13,7 +14,6 @@ export const appRoutes: Route[] = [
         loadComponent: () => import('./pages/home/home'),
       },
       ...componentsRoutes,
-      ...demosRoutes,
     ],
   },
 ];
