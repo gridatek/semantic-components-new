94b0bd985886dd1247b2f2d3c55e172daef43049
kgridou <32600911+kgridou@users.noreply.github.com>
2026-01-23 16:22:46 +0100

    wip

--- libs/ui/src/lib/components/number-input/index.ts
+++ libs/ui/src/lib/components/number-input/index.ts
@@ -1,8 +1,5 @@
-export {
-  SC_NUMBER_INPUT,
-  ScNumberInput,
-  ScNumberInputField,
-  ScNumberInputIncrement,
-  ScNumberInputDecrement,
-  ScNumberInputStepper,
-} from './sc-number-input';
+export * from './number-input';
+export * from './number-input-decrement';
+export * from './number-input-field';
+export * from './number-input-increment';
+export * from './number-input-stepper';
