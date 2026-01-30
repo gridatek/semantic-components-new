8a13e7032c4560dc7138d6ad94ceef0b6172a745
kgridou <32600911+kgridou@users.noreply.github.com>
2026-01-30 12:42:13 +0100

    wip

--- libs/ui/src/lib/components/theme-toggle/theme-toggle.ts
+++ libs/ui/src/lib/components/theme-toggle/theme-toggle.ts
@@ -57,7 +57,7 @@ export class ScThemeToggle {
       'ring-offset-background transition-colors',
       'focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2',
       'disabled:pointer-events-none disabled:opacity-50',
-      '[&_svg]:pointer-events-none [&_svg]:size-5 [&_svg]:shrink-0',
+      '[&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0',
       variantStyles[this.variant()],
       sizeStyles[this.size()],
       this.classInput(),
