71bc44f9fa93b3a35412e930ef7ba18f2342fef7
kgridou <32600911+kgridou@users.noreply.github.com>
2026-01-30 14:36:38 +0100

    wip

--- libs/ui/src/lib/components/sidebar-x/sidebar.ts
+++ libs/ui/src/lib/components/sidebar-x/sidebar.ts
@@ -58,12 +58,12 @@ export class ScxSidebar {
     const variant = this.variant();
 
     return cn(
-      'relative h-svh w-[--sidebar-width] bg-transparent transition-[width] duration-200 ease-linear',
+      'relative w-[var(--sidebar-width)] bg-transparent transition-[width] duration-200 ease-linear',
       'group-data-[collapsible=offcanvas]:w-0',
       'group-data-[side=right]:rotate-180',
       variant === 'floating' || variant === 'inset'
         ? 'group-data-[collapsible=icon]:w-[calc(var(--sidebar-width-icon)_+_theme(spacing.4))]'
-        : 'group-data-[collapsible=icon]:w-[--sidebar-width-icon]',
+        : 'group-data-[collapsible=icon]:w-[var(--sidebar-width-icon)]',
     );
   });
 
@@ -72,24 +72,20 @@ export class ScxSidebar {
     const variant = this.variant();
 
     return cn(
-      'fixed inset-y-0 z-10 hidden h-svh w-[--sidebar-width] transition-[left,right,width] duration-200 ease-linear md:flex',
+      'fixed inset-y-0 z-10 hidden h-svh w-[var(--sidebar-width)] transition-[left,right,width] duration-200 ease-linear md:flex',
       side === 'left'
         ? 'left-0 group-data-[collapsible=offcanvas]:left-[calc(var(--sidebar-width)*-1)]'
         : 'right-0 group-data-[collapsible=offcanvas]:right-[calc(var(--sidebar-width)*-1)]',
       variant === 'floating' || variant === 'inset'
         ? 'p-2 group-data-[collapsible=icon]:w-[calc(var(--sidebar-width-icon)_+_theme(spacing.4)_+2px)]'
-        : 'group-data-[collapsible=icon]:w-[--sidebar-width-icon] group-data-[side=left]:border-r group-data-[side=right]:border-l',
-      side === 'right' && 'group-data-[side=right]:rotate-180',
+        : 'group-data-[collapsible=icon]:w-[var(--sidebar-width-icon)] group-data-[side=left]:border-r group-data-[side=right]:border-l',
     );
   });
 
-  protected readonly innerClass = computed(() => {
-    const variant = this.variant();
-    const side = this.side();
-
-    return cn(
-      'flex h-full w-full flex-col bg-sidebar group-data-[variant=floating]:rounded-lg group-data-[variant=floating]:border group-data-[variant=floating]:border-sidebar-border group-data-[variant=floating]:shadow',
-      side === 'right' && 'group-data-[side=right]:rotate-180',
-    );
-  });
+  protected readonly innerClass = computed(() =>
+    cn(
+      'flex h-full w-full flex-col bg-sidebar',
+      'group-data-[variant=floating]:rounded-lg group-data-[variant=floating]:shadow-sm group-data-[variant=floating]:ring-1 group-data-[variant=floating]:ring-sidebar-border',
+    ),
+  );
 }
