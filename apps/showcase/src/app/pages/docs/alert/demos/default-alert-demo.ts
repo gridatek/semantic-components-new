21e5bd4789eeae1fe2f0db846bbf6534d1da6305
kgridou <32600911+kgridou@users.noreply.github.com>
2026-02-01 19:13:52 +0100

    wip

--- apps/showcase/src/app/pages/docs/alert/demos/default-alert-demo.ts
+++ apps/showcase/src/app/pages/docs/alert/demos/default-alert-demo.ts
@@ -4,28 +4,14 @@ import {
   ScAlertDescription,
   ScAlertTitle,
 } from '@semantic-components/ui';
+import { SiAlertCircleIcon } from '@semantic-icons/lucide-icons';
 
 @Component({
   selector: 'app-default-alert-demo',
-  imports: [ScAlert, ScAlertDescription, ScAlertTitle],
+  imports: [ScAlert, ScAlertDescription, ScAlertTitle, SiAlertCircleIcon],
   template: `
     <div sc-alert>
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width="24"
-        height="24"
-        viewBox="0 0 24 24"
-        fill="none"
-        stroke="currentColor"
-        stroke-width="2"
-        stroke-linecap="round"
-        stroke-linejoin="round"
-        class="size-4"
-      >
-        <circle cx="12" cy="12" r="10" />
-        <path d="M12 16v-4" />
-        <path d="M12 8h.01" />
-      </svg>
+      <svg si-alert-circle-icon></svg>
       <h5 sc-alert-title>Heads up!</h5>
       <div sc-alert-description>
         You can add components to your app using the cli.
--- apps/showcase/src/app/pages/docs/alert/demos/destructive-alert-demo.ts
+++ apps/showcase/src/app/pages/docs/alert/demos/destructive-alert-demo.ts
@@ -4,28 +4,14 @@ import {
   ScAlertDescription,
   ScAlertTitle,
 } from '@semantic-components/ui';
+import { SiAlertTriangleIcon } from '@semantic-icons/lucide-icons';
 
 @Component({
   selector: 'app-destructive-alert-demo',
-  imports: [ScAlert, ScAlertDescription, ScAlertTitle],
+  imports: [ScAlert, ScAlertDescription, ScAlertTitle, SiAlertTriangleIcon],
   template: `
     <div sc-alert variant="destructive">
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width="24"
-        height="24"
-        viewBox="0 0 24 24"
-        fill="none"
-        stroke="currentColor"
-        stroke-width="2"
-        stroke-linecap="round"
-        stroke-linejoin="round"
-        class="size-4"
-      >
-        <circle cx="12" cy="12" r="10" />
-        <line x1="12" x2="12" y1="8" y2="12" />
-        <line x1="12" x2="12.01" y1="16" y2="16" />
-      </svg>
+      <svg si-alert-triangle-icon></svg>
       <h5 sc-alert-title>Error</h5>
       <div sc-alert-description>
         Your session has expired. Please log in again.
