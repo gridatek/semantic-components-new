d0020b5b680158acd2d5f8830378405bac60d9fd
kgridou <32600911+kgridou@users.noreply.github.com>
2026-02-01 19:21:14 +0100

    wip

--- apps/showcase/src/app/pages/docs/alert/demos/destructive-alert-demo-container.ts
+++ apps/showcase/src/app/pages/docs/alert/demos/destructive-alert-demo-container.ts
@@ -14,36 +14,31 @@ import { DestructiveAlertDemo } from './destructive-alert-demo';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class DestructiveAlertDemoContainer {
-  readonly code = `import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { ScAlert, ScAlertDescription, ScAlertTitle } from '@semantic-components/ui';
+  readonly code = `import {
+  ChangeDetectionStrategy,
+  Component,
+  ViewEncapsulation,
+} from '@angular/core';
+import {
+  ScAlert,
+  ScAlertDescription,
+  ScAlertTitle,
+} from '@semantic-components/ui';
+import { SiAlertTriangleIcon } from '@semantic-icons/lucide-icons';
 
 @Component({
   selector: 'app-destructive-alert-demo',
-  imports: [ScAlert, ScAlertDescription, ScAlertTitle],
+  imports: [ScAlert, ScAlertDescription, ScAlertTitle, SiAlertTriangleIcon],
   template: \`
     <div sc-alert variant="destructive">
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width="24"
-        height="24"
-        viewBox="0 0 24 24"
-        fill="none"
-        stroke="currentColor"
-        stroke-width="2"
-        stroke-linecap="round"
-        stroke-linejoin="round"
-        class="size-4"
-      >
-        <circle cx="12" cy="12" r="10" />
-        <line x1="12" x2="12" y1="8" y2="12" />
-        <line x1="12" x2="12.01" y1="16" y2="16" />
-      </svg>
+      <svg si-alert-triangle-icon></svg>
       <h5 sc-alert-title>Error</h5>
       <div sc-alert-description>
         Your session has expired. Please log in again.
       </div>
     </div>
   \`,
+  encapsulation: ViewEncapsulation.None,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class DestructiveAlertDemo {}`;
