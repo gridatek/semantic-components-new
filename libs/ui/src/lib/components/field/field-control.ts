90f16fd5ceced62e6350c672149ffb90e909e2d9
kgridou <32600911+kgridou@users.noreply.github.com>
2026-02-03 07:39:18 +0100

    wip

--- libs/ui/src/lib/components/field/field-control.ts
+++ libs/ui/src/lib/components/field/field-control.ts
@@ -1,15 +1,33 @@
-import { Directive, inject } from '@angular/core';
+import { computed, Directive, inject, input } from '@angular/core';
 import { SC_FIELD_TOKEN } from './field';
 
 @Directive({
   selector:
     'input[sc-field-control], textarea[sc-field-control], select[sc-field-control]',
   host: {
-    '[attr.id]': 'field.id()',
-    '[attr.aria-invalid]': 'field.invalid()',
-    '[attr.disabled]': 'field.disabled() || null',
+    '[attr.id]': 'controlId()',
+    '[attr.aria-invalid]': 'ariaInvalid()',
+    '[attr.disabled]': 'controlDisabled()',
   },
 })
 export class ScFieldControl {
-  readonly field = inject(SC_FIELD_TOKEN);
+  private readonly field = inject(SC_FIELD_TOKEN, { optional: true });
+
+  readonly id = input<string>();
+  readonly invalid = input<boolean>();
+  readonly disabled = input<boolean>();
+
+  protected readonly controlId = computed(() => {
+    return this.id() ?? this.field?.id();
+  });
+
+  protected readonly ariaInvalid = computed(() => {
+    const invalid = this.invalid() ?? this.field?.invalid();
+    return invalid ?? null;
+  });
+
+  protected readonly controlDisabled = computed(() => {
+    const disabled = this.disabled() ?? this.field?.disabled();
+    return disabled || null;
+  });
 }
