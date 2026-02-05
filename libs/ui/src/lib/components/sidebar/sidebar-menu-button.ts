738d68747f1d412aba4d65013bbab4533f8381cb
kgridou <32600911+kgridou@users.noreply.github.com>
2026-02-05 23:02:40 +0100

    wip

--- libs/ui/src/lib/components/sidebar/sidebar-menu-button.ts
+++ libs/ui/src/lib/components/sidebar/sidebar-menu-button.ts
@@ -1,11 +1,32 @@
-import { Directive, computed, input } from '@angular/core';
+import { computed, Directive, input } from '@angular/core';
+import { cva, VariantProps } from 'class-variance-authority';
 import { cn } from '../../utils';
 
-const sizeStyles: Record<'default' | 'sm' | 'lg', string> = {
-  default: 'h-8 text-sm',
-  sm: 'h-7 text-xs',
-  lg: 'h-12 text-sm group-data-[collapsible=icon]:!p-0',
-};
+const sidebarMenuButtonVariants = cva(
+  'ring-sidebar-ring hover:bg-sidebar-accent hover:text-sidebar-accent-foreground active:bg-sidebar-accent active:text-sidebar-accent-foreground data-active:bg-sidebar-accent data-active:text-sidebar-accent-foreground data-open:hover:bg-sidebar-accent data-open:hover:text-sidebar-accent-foreground gap-2 rounded-md p-2 text-left text-sm transition-[width,height,padding] group-has-data-[sidebar=menu-action]/menu-item:pr-8 group-data-[collapsible=icon]:size-8! group-data-[collapsible=icon]:p-2! focus-visible:ring-2 data-active:font-medium peer/menu-button flex w-full items-center overflow-hidden outline-hidden group/menu-button disabled:pointer-events-none disabled:opacity-50 aria-disabled:pointer-events-none aria-disabled:opacity-50 [&>span:last-child]:truncate [&_svg]:size-4 [&_svg]:shrink-0',
+  {
+    variants: {
+      variant: {
+        default: 'hover:bg-sidebar-accent hover:text-sidebar-accent-foreground',
+        outline:
+          'bg-background hover:bg-sidebar-accent hover:text-sidebar-accent-foreground shadow-[0_0_0_1px_hsl(var(--sidebar-border))] hover:shadow-[0_0_0_1px_hsl(var(--sidebar-accent))]',
+      },
+      size: {
+        default: 'h-8 text-sm',
+        sm: 'h-7 text-xs',
+        lg: 'h-12 text-sm group-data-[collapsible=icon]:p-0!',
+      },
+    },
+    defaultVariants: {
+      variant: 'default',
+      size: 'default',
+    },
+  },
+);
+
+export type ScSidebarMenuButtonVariants = VariantProps<
+  typeof sidebarMenuButtonVariants
+>;
 
 @Directive({
   selector: 'button[sc-sidebar-menu-button], a[sc-sidebar-menu-button]',
@@ -19,15 +40,15 @@ const sizeStyles: Record<'default' | 'sm' | 'lg', string> = {
 export class ScSidebarMenuButton {
   readonly classInput = input<string>('', { alias: 'class' });
 
-  readonly size = input<'default' | 'sm' | 'lg'>('default');
+  readonly variant = input<ScSidebarMenuButtonVariants['variant']>('default');
+  readonly size = input<ScSidebarMenuButtonVariants['size']>('default');
   readonly isActive = input<boolean>(false);
   readonly tooltip = input<string>();
 
   protected readonly class = computed(() => {
     const size = this.size();
     return cn(
-      'peer/menu-button flex w-full items-center gap-2 overflow-hidden rounded-md p-2 text-left text-sm outline-none ring-sidebar-ring transition-[width,height,padding] hover:bg-sidebar-accent hover:text-sidebar-accent-foreground focus-visible:ring-2 active:bg-sidebar-accent active:text-sidebar-accent-foreground disabled:pointer-events-none disabled:opacity-50 group-has-[[data-sidebar=menu-action]]/menu-item:pr-8 aria-disabled:pointer-events-none aria-disabled:opacity-50 data-[active=true]:bg-sidebar-accent data-[active=true]:font-medium data-[active=true]:text-sidebar-accent-foreground data-[state=open]:hover:bg-sidebar-accent data-[state=open]:hover:text-sidebar-accent-foreground group-data-[collapsible=icon]:!size-8 group-data-[collapsible=icon]:!p-2 [&>span:last-child]:truncate [&>svg]:size-4 [&>svg]:shrink-0',
-      sizeStyles[size],
+      sidebarMenuButtonVariants({ variant: this.variant(), size }),
       this.classInput(),
     );
   });
