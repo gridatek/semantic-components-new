ba16b753ab7c0d8a17aa733176e445d99f9dd5c6
kgridou <32600911+kgridou@users.noreply.github.com>
2026-02-06 15:52:35 +0100

    wip

--- libs/ui/src/lib/components/pagination/pagination-link.ts
+++ libs/ui/src/lib/components/pagination/pagination-link.ts
@@ -8,6 +8,7 @@ import {
 } from '@angular/core';
 import { cn } from '../../utils';
 import { ScPagination } from './pagination';
+import { buttonVariants, ScButtonVariants } from '../button';
 
 @Directive({
   selector: 'a[sc-pagination-link], button[sc-pagination-link]',
@@ -26,7 +27,11 @@ export class ScPaginationLink {
 
   readonly classInput = input<string>('', { alias: 'class' });
   readonly page = input<number>();
-  readonly size = input<'default' | 'sm' | 'lg' | 'icon'>('icon');
+
+  readonly variant = computed(() => (this.isActive() ? 'outline' : 'ghost'));
+
+  readonly size = input<ScButtonVariants['size']>('icon');
+
   readonly disabled = input<boolean, unknown>(false, {
     transform: booleanAttribute,
   });
@@ -41,24 +46,12 @@ export class ScPaginationLink {
     return pageNum === this.pagination.currentPage();
   });
 
-  protected readonly class = computed(() => {
-    const sizeClasses = {
-      default: 'h-10 px-4 py-2',
-      sm: 'h-9 px-3',
-      lg: 'h-11 px-8',
-      icon: 'size-10',
-    };
-
-    return cn(
-      'inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2',
-      'hover:bg-accent hover:text-accent-foreground',
-      'aria-disabled:pointer-events-none aria-disabled:opacity-50',
-      '[&_svg]:size-4',
-      this.isActive() && 'border border-input bg-background',
-      sizeClasses[this.size()],
+  protected readonly class = computed(() =>
+    cn(
+      buttonVariants({ variant: this.variant(), size: this.size() }),
       this.classInput(),
-    );
-  });
+    ),
+  );
 
   onClick(event: Event): void {
     event.preventDefault();
