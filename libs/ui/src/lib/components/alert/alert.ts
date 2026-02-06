0143f40438066ec4759c77e9b720e980a5eb21a8
kgridou <32600911+kgridou@users.noreply.github.com>
2026-02-06 19:58:54 +0100

    wip

--- libs/ui/src/lib/components/alert/alert.ts
+++ libs/ui/src/lib/components/alert/alert.ts
@@ -1,6 +1,25 @@
 import { computed, Directive, input } from '@angular/core';
 import { cn } from '../../utils';
-import { alertVariants, ScAlertVariants } from './alert-variants';
+
+import { cva, type VariantProps } from 'class-variance-authority';
+
+const alertVariants = cva(
+  "grid gap-0.5 rounded-lg border px-2.5 py-2 text-left text-sm has-data-[slot=alert-action]:relative has-data-[slot=alert-action]:pr-18 has-[>svg]:grid-cols-[auto_1fr] has-[>svg]:gap-x-2 *:[svg]:row-span-2 *:[svg]:translate-y-0.5 *:[svg]:text-current *:[svg:not([class*='size-'])]:size-4 w-full relative group/alert",
+  {
+    variants: {
+      variant: {
+        default: 'bg-card text-card-foreground',
+        destructive:
+          'text-destructive bg-card *:data-[slot=alert-description]:text-destructive/90 *:[svg]:text-current',
+      },
+    },
+    defaultVariants: {
+      variant: 'default',
+    },
+  },
+);
+
+export type ScAlertVariants = VariantProps<typeof alertVariants>;
 
 @Directive({
   selector: '[sc-alert]',
--- libs/ui/src/lib/components/alert/index.ts
+++ libs/ui/src/lib/components/alert/index.ts
@@ -1,5 +1,4 @@
 export * from './alert';
 export * from './alert-title';
 export * from './alert-description';
-export * from './alert-variants';
 export * from './alert-action';
