c118fdb04eaadb998c814f14bbd93a7ad8890580
kgridou <32600911+kgridou@users.noreply.github.com>
2026-02-07 14:28:32 +0100

    wip

--- libs/ui/src/lib/components/carousel/carousel-previous.ts
+++ libs/ui/src/lib/components/carousel/carousel-previous.ts
@@ -8,6 +8,7 @@ import {
 } from '@angular/core';
 import { cn } from '../../utils';
 import { ScCarousel } from './carousel';
+import { buttonVariants, ScButtonVariants } from '../button';
 
 @Component({
   selector: 'button[sc-carousel-previous]',
@@ -29,16 +30,14 @@ export class ScCarouselPrevious {
 
   readonly classInput = input<string>('', { alias: 'class' });
 
+  readonly variant = input<ScButtonVariants['variant']>('outline');
+  readonly size = input<ScButtonVariants['size']>('icon-sm');
+
   protected readonly class = computed(() => {
     const isHorizontal = this.carousel.orientation() === 'horizontal';
     return cn(
+      buttonVariants({ variant: this.variant(), size: this.size() }),
       'rounded-full absolute touch-manipulation',
-      'size-8 inline-flex items-center justify-center',
-      'border border-input bg-background',
-      'hover:bg-accent hover:text-accent-foreground',
-      'disabled:pointer-events-none disabled:opacity-50',
-      '[&_svg]:size-4',
-      'focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2',
       isHorizontal
         ? 'top-1/2 -left-12 -translate-y-1/2'
         : '-top-12 left-1/2 -translate-x-1/2 rotate-90',
