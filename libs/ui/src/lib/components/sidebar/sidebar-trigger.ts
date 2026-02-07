2072ea75e2f481c3b84a77bbada7286c30cf5a64
kgridou <32600911+kgridou@users.noreply.github.com>
2026-02-07 15:45:26 +0100

    wip

--- libs/ui/src/lib/components/sidebar/sidebar-trigger.ts
+++ libs/ui/src/lib/components/sidebar/sidebar-trigger.ts
@@ -1,11 +1,13 @@
 import { Directive, computed, inject, input } from '@angular/core';
 import { cn } from '../../utils';
 import { ScSidebarState } from './sidebar-state.service';
+import { buttonVariants, ScButtonVariants } from '../button';
 
 @Directive({
   selector: 'button[sc-sidebar-trigger]',
   host: {
     'data-slot': 'sidebar-trigger',
+    type: 'button',
     '[class]': 'class()',
     '(click)': 'onClick($event)',
   },
@@ -15,7 +17,15 @@ export class ScSidebarTrigger {
 
   readonly classInput = input<string>('', { alias: 'class' });
 
-  protected readonly class = computed(() => cn('', this.classInput()));
+  readonly variant = input<ScButtonVariants['variant']>('ghost');
+  readonly size = input<ScButtonVariants['size']>('icon-sm');
+
+  protected readonly class = computed(() =>
+    cn(
+      buttonVariants({ variant: this.variant(), size: this.size() }),
+      this.classInput(),
+    ),
+  );
 
   protected onClick(event: MouseEvent): void {
     event.preventDefault();
