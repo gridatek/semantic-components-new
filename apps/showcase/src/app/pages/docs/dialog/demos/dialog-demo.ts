d060faf7d67662e46032dc916dea6f1566d60a5c
kgridou <32600911+kgridou@users.noreply.github.com>
2026-02-07 11:22:17 +0100

    wip

--- apps/showcase/src/app/pages/docs/dialog/demos/dialog-demo.ts
+++ apps/showcase/src/app/pages/docs/dialog/demos/dialog-demo.ts
@@ -39,30 +39,33 @@ import {
       <button sc-dialog-trigger sc-button variant="outline">Open Dialog</button>
       <div sc-dialog-portal>
         <ng-template>
-          <div sc-dialog class="sm:max-w-sm">
-            <div sc-dialog-header>
-              <h2 sc-dialog-title>Edit profile</h2>
-              <p sc-dialog-description>
-                Make changes to your profile here. Click save when you're done.
-              </p>
-            </div>
-            <div sc-field-group>
-              <div sc-field orientation="horizontal">
-                <label sc-label>Name</label>
-                <input sc-input value="Pedro Duarte" />
+          <form>
+            <div sc-dialog class="sm:max-w-sm">
+              <div sc-dialog-header>
+                <h2 sc-dialog-title>Edit profile</h2>
+                <p sc-dialog-description>
+                  Make changes to your profile here. Click save when you're
+                  done.
+                </p>
               </div>
-              <div sc-field orientation="horizontal">
-                <label sc-label>Username</label>
-                <input sc-input value="@peduarte" />
+              <div sc-field-group>
+                <div sc-field orientation="horizontal">
+                  <label sc-label>Name</label>
+                  <input sc-input value="Pedro Duarte" />
+                </div>
+                <div sc-field orientation="horizontal">
+                  <label sc-label>Username</label>
+                  <input sc-input value="@peduarte" />
+                </div>
+              </div>
+              <div sc-dialog-footer>
+                <button sc-dialog-close sc-button variant="outline">
+                  Cancel
+                </button>
+                <button sc-button type="submit">Save changes</button>
               </div>
             </div>
-            <div sc-dialog-footer>
-              <button sc-dialog-close sc-button variant="outline">
-                Cancel
-              </button>
-              <button sc-button type="submit">Save changes</button>
-            </div>
-          </div>
+          </form>
         </ng-template>
       </div>
     </div>
